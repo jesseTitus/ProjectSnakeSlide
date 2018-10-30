a92bd2917635ab005cf0d86ad144654ac481c169
Jesse Titus <jessejtitus@gmail.com>
2018-10-30 18:41:17 -0400

    Added mvmt and camFollow

---
+++ SnakeSlide/Assets/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+
+    
+    
+    void Update ()
+    {
+        transform.position = PlayerTransform.position + new Vector3(0,0,-10);
+    }
+}
